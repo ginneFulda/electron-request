e6c1c1aa40dc93eefd882cdeb563f0746d8ee1ac
islizeqiang <islizeqiang@gmail.com>
2021-06-20 18:59:41 +0800

    feat: export type Response

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import Request from './Request';
-import type { Options, Response } from './typings.d';
+import type { Options, Response, ProgressInfo } from './typings.d';
 
 const main = (requestURL: string, options: Options = {}): Promise<Response> => {
   const request = new Request({ requestURL, ...options });
   return request.send();
 };
 
-export type { ProgressInfo } from './typings.d';
+export type { Response, ProgressInfo };
 
 export default main;
