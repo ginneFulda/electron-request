32150fd38129375468e83dc73bddbd0f16d2a4cf
islizeqiang <islizeqiang@gmail.com>
2021-07-22 21:01:48 +0800

    chore: remove whitespace

--- rollup.config.js
+++ rollup.config.js
@@ -5,39 +5,39 @@ import dts from 'rollup-plugin-dts';
 import pkgConfig from './package.json';
 
 const resolveTypescriptPaths = () =>
-    typescriptPaths({
-        preserveExtensions: true,
-    });
+  typescriptPaths({
+    preserveExtensions: true,
+  });
 
 const external = (id) => {
-    if (isBuiltin(id)) {
-        return true;
-    }
-    return Reflect.has(pkgConfig.dependencies || {}, id);
+  if (isBuiltin(id)) {
+    return true;
+  }
+  return Reflect.has(pkgConfig.dependencies || {}, id);
 };
 
 export default [
-    {
-        input: 'src/index.ts',
-        plugins: [resolveTypescriptPaths(), typescript()],
-        output: [
-            { file: pkgConfig.module, format: 'es' },
-            { file: pkgConfig.main, format: 'cjs', exports: 'named' },
-        ],
-        external,
-    },
-    {
-        input: 'src/index.ts',
-        plugins: [
-            resolveTypescriptPaths(),
-            dts({
-                respectExternal: true,
-                compilerOptions: {
-                    removeComments: false,
-                },
-            }),
-        ],
-        output: [{ file: pkgConfig.types, format: 'es' }],
-        external: (id) => isBuiltin(id),
-    },
+  {
+    input: 'src/index.ts',
+    plugins: [resolveTypescriptPaths(), typescript()],
+    output: [
+      { file: pkgConfig.module, format: 'es' },
+      { file: pkgConfig.main, format: 'cjs', exports: 'named' },
+    ],
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [
+      resolveTypescriptPaths(),
+      dts({
+        respectExternal: true,
+        compilerOptions: {
+          removeComments: false,
+        },
+      }),
+    ],
+    output: [{ file: pkgConfig.types, format: 'es' }],
+    external: (id) => isBuiltin(id),
+  },
 ];
